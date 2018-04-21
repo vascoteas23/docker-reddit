ba65d7a2c47890b44d9589165e68126f6742e2a5
vascoteas23 <v.coelho@campus.fct.unl.pt>
2018-04-21 23:59:59 +0100

    deleting the waiting for zookeper

--- install/install_zookeeper.sh
+++ install/install_zookeeper.sh
@@ -27,6 +27,6 @@ source $RUNDIR/install.cfg
 sudo apt-get install $APTITUDE_OPTIONS zookeeperd
 
 echo "Waiting for ZooKeeper to be available..."
-while ! nc -vz localhost 2181; do
-    sleep 1
-done
+#while ! nc -vz localhost 2181; do
+#    sleep 1
+#done
