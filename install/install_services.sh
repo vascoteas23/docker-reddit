c33a78f312bbbe09d7a78f1ab32bd9f9208c969d
vascoteas23 <v.coelho@campus.fct.unl.pt>
2018-04-22 00:37:20 +0100

    removing while from install-services.sh

--- install/install_services.sh
+++ install/install_services.sh
@@ -50,8 +50,8 @@ echo "Waiting for services to be available, see source for port meanings..."
 # 11211 - memcache
 # 5432 - postgres
 # 5672 - rabbitmq
-for port in 11211 5432 5672; do
-    while ! nc -vz localhost $port; do
-        sleep 1
-    done
-done
+#for port in 11211 5432 5672; do
+#    while ! nc -vz localhost $port; do
+#        sleep 1
+#    done
+#done
